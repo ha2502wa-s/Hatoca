f3369acda4930e0d6feb40256f546f620c1bb4f9
tovehultin <32096517+tovehultin@users.noreply.github.com>
2018-02-15 11:02:02 +0100

    databas koppling

--- Hatoca/BookingApp.Designer.cs
+++ Hatoca/BookingApp.Designer.cs
@@ -4,13 +4,14 @@
 	{
 
 		public static void Main(string[] args)
-		{
-		}
+        {
+        }
 
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
 		/// <summary>
 		/// Clean up any resources being used.
